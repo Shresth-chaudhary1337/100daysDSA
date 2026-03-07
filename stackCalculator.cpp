36e329f7f3c2217323723be6c4c2643577f96ec8
Shresth-chaudhary1337 <chaudharyshresth07@gmail.com>
2026-03-07 13:53:06 +0530

    Add stackCalculator.cpp

---
+++ stackCalculator.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int calculate(string s) {
+    stack<int> st;
+    int num = 0;
+    char op = '+';
+
+    for (int i = 0; i < s.length(); i++) {
+
+        if (isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+        }
+
+        if ((!isdigit(s[i]) && s[i] != ' ') || i == s.length() - 1) {
+
+            if (op == '+')
+                st.push(num);
+
+            else if (op == '-')
+                st.push(-num);
+
+            else if (op == '*') {
+                int top = st.top();
+                st.pop();
+                st.push(top * num);
+            }
+
+            else if (op == '/') {
+                int top = st.top();
+                st.pop();
+                st.push(top / num);
+            }
+
+            op = s[i];
+            num = 0;
+        }
+    }
+
+    int result = 0;
+
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+
+    return result;
+}
+
+int main() {
+    string s;
+
+    cout << "Enter expression: ";
+    getline(cin, s);
+
+    cout << "Result = " << calculate(s);
+
+    return 0;
+}
